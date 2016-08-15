838aa6bb27d3b34a43588047d203aa87369c12ee
tmoffat <tsmoffat@gmail.com>
2016-08-15 16:55:12 +0100

    More testing, trying to get two element solutions working

--- Testing.py
+++ Testing.py
@@ -9,7 +9,7 @@ class main:
         s180 = wb.get_sheet_by_name('180')
         s90 = wb.get_sheet_by_name('90')
         s45 = wb.get_sheet_by_name('45')
-        s225 = wb.get_sheet_by_name('225')
+        s225 = wb.get_sheet_by_name('22.5')
 
         nb = input("What frequency are you using?")
         while nb != "24GHz" and nb != "28GHz" and nb != "32GHz":
@@ -17,28 +17,28 @@ class main:
             print(nb)
         if (nb == '24GHz'):
             phase = 1
-            Att = 5
+            att = 5
             DSAState = 1
             DSAS11 = 2
             DSAS21 = 3
             DSAS22 = 4
         elif (nb == '28GHz'):
             phase = 2
-            Att = 6
+            att = 6
             DSAState = 6
             DSAS11 = 7
             DSAS21 = 8
             DSAS22 = 9
         elif (nb == '32GHz'):
             phase = 3
-            Att = 7
+            att = 7
             DSAState = 11
             DSAS11 = 12
             DSAS21 = 13
             DSAS22 = 14
         else:
             phase = 0
-            Att = 0
+            att = 0
             DSAState = 0
             DSAS11 = 0
             DSAS21 = 0
@@ -47,23 +47,69 @@ class main:
         list180 = []
         for row in s180.iter_rows(row_offset=2):
             if (row[phase].value != None):
-                print(row[phase].value)
                 list180.append(float(row[phase].value))
-
+        set180 = set(list180)
         list90 = []
         for row in s90.iter_rows(row_offset=2):
             if (row[phase].value != None):
-                print(row[phase].value)
                 list90.append(float(row[phase].value))
-
+        set90 = set(list90)
         list45 = []
         for row in s45.iter_rows(row_offset=2):
             if (row[phase].value != None):
-                print(row[phase].value)
-                list45.append(float(row[phase].value))
 
+                list45.append(float(row[phase].value))
+        set45 = set(list45)
         list225 = []
         for row in s225.iter_rows(row_offset=2):
             if (row[phase].value != None):
-                print(row[phase].value)
+
                 list225.append(float(row[phase].value))
+        set225 = set(list225)
+        targetphase = float(input("What phase do you want to achieve?"))
+        targetatt = input("What attenuation do you want to achieve?")
+        if targetphase in set180:
+            print("Found it!")
+            for row in s180.iter_rows():
+                if row[phase].value == targetphase:
+                    optimalrow = int(row[0].value)
+                    att180 = float(row[att].value)
+            print("Optimal solution found, it is state " + str(optimalrow))
+            print(att180)
+
+        elif targetphase in set90:
+            print("Found it!")
+            for row in s90.iter_rows():
+                if row[phase].value == targetphase:
+                    optimalrow = int(row[0].value)
+                    att90 = float(row[att].value)
+            print("Optimal solution found, it is state " + str(optimalrow))
+            print(att90)
+        elif targetphase in set45:
+            print("Found it!")
+            for row in s45.iter_rows():
+                if row[phase].value == targetphase:
+                    optimalrow = int(row[0].value)
+                    att45 = float(row[att].value)
+            print("Optimal solution found, it is state "  + str(optimalrow))
+            print(att45)
+
+        elif targetphase in set225
+            print("Found it!")
+            for row in list225.iter_rows():
+                if row[phase].value == targetphase:
+                    optimalrow = int(row[0].value)
+                    att225 = float(row[att].value)
+            print("Optimal solution found, it is state " + str(optimalrow))
+            print(att225)
+
+
+        else:
+            print("Not found")
+
+
+    def twoelementsum(self, ):
+
+
+m = main()
+m.main()
